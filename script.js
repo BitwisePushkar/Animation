65b633b1157a125a74cb80c2c131d9774bf7eb6c
Pushar Singhal <pusinghal010@gmail.com>
2025-09-19 15:39:12 +0530

    added file

---
+++ script.js
@@ -0,0 +1,15 @@
+const canvas=document.getElementById("canvas1");
+const ctx=canvas.getContext('2d');
+canvas.width=800;
+canvas.height=500;
+
+let score=1;
+let gameFrame=0;
+ctx.font="50px Georgia";
+
+const mouse={
+  X: canvas.width/2,
+  Y: canvas.height/2,
+  click: false
+}
+
